b1f8c64c5fe99d7e69ffc6c78ad448ed688e6010
uchiyama chiho <chichi0127ho@gmail.com>
2018-10-08 01:52:09 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -7,5 +9,4 @@ Rails.application.routes.draw do
   
   get 'static_pages/contact'
 
-  root 'application#hello'
 end
