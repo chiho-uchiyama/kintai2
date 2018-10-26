306058d0692d78723079544fab53048e080d44f3
uchiyama chiho <chichi0127ho@gmail.com>
2018-10-26 06:04:42 +0000

    8.2.5 complit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,24 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   private
 
     def user_params
@@ -25,4 +26,3 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
-
