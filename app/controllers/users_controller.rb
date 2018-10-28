eaa3c6263d2df1916972ad4d16174f0c43c3563d
uchiyama chiho <chichi0127ho@gmail.com>
2018-10-28 22:54:23 +0000

    10.3.5 complit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-  
   def show
     @user = User.find(params[:id])
   end
@@ -27,6 +23,10 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def update
