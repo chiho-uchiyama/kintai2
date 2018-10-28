925d2e892ff09db3a906daa15b14f431dc1d6e07
uchiyama chiho <chichi0127ho@gmail.com>
2018-10-28 04:46:03 +0000

    10.2.2 complit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
-
+  before_action :correct_user,   only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -48,6 +49,12 @@ class UsersController < ApplicationController
       unless logged_in?
         flash[:danger] = "Please log in."
         redirect_to login_url
+      end
     end
-  end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
