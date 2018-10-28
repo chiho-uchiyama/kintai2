0c03f756068e68efdcb626c061fa44f1ead9a834
uchiyama chiho <chichi0127ho@gmail.com>
2018-10-28 05:28:06 +0000

    10.3.1 complit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,9 +7,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
   
   test "should redirect edit when not logged in" do
