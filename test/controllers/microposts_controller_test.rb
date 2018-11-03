5d886109f94dd85954ef0084b36409ce4aa2eada
uchiyama chiho <chichi0127ho@gmail.com>
2018-11-03 07:33:46 +0000

    13.3.5 complit

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @micropost = microposts(:orange)
   end
@@ -18,5 +18,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
