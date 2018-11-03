06b8e45d7bed256690327726093587058829ad4f
uchiyama chiho <chichi0127ho@gmail.com>
2018-11-03 03:42:28 +0000

    13.1.4 complit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,8 +4,8 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-    password: "foobar", password_confirmation: "foobar")
-  end
+                     password: "foobar", password_confirmation: "foobar")
+  end 
 
   test "should be valid" do
     assert @user.valid?
@@ -50,5 +50,13 @@ class UserTest < ActiveSupport::TestCase
   
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
